74e80bdddcbbd0aeaee1f286e27a829de4e22147
Matvijivan <matvijivan>
2021-06-20 01:59:39 +0300

    add photo

--- app/src/main/java/com/example/thesalary/Constans.kt
+++ app/src/main/java/com/example/thesalary/Constans.kt
@@ -1,22 +1,25 @@
 package com.example.thesalary
 
 object Constans {
-    const val  Driver_Salary = 100
-    const val  Enginer_Salary = 1000
-    const val  Director_Salary = 10000
+    const val  Driver_Salary = 10
+    const val  Enginer_Salary = 100
+    const val  Director_Salary = 1000
     const val  PMHead_Salary = 10000
-    const val  SuperHead_Salary = 10000
+    const val  SuperHead_Salary = 100000
 
     const val Director = "Ivan"
     const val Enginer = "Kola"
     const val Driver = "Vasia"
     const val PMHead = "Lesha"
     const val SuperHead = "Sasha"
+    const val AllPerson = "Ivan,Kola,Vasia,Lesha,Sasha"
 
     const val  Director_Password = "1"
-    const val  Enginer_Password = "1"
-    const val  Driver_Password = "1"
-    const val  PMHead_Password = "1"
-    const val  SuperHead_Password = "1"
+    const val  Enginer_Password = "2"
+    const val  Driver_Password = "3"
+    const val  PMHead_Password = "4"
+    const val  SuperHead_Password = "5"
+
+
 
 }
--- app/src/main/java/com/example/thesalary/MainActivity.kt
+++ app/src/main/java/com/example/thesalary/MainActivity.kt
@@ -13,60 +13,87 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+        bindingClass.textViewAllName.text = "You name ${Constans.AllPerson}"
         bindingClass.buttonSalary.setOnClickListener(){
 
             val resultValue = bindingClass.tPersonName.text.toString()
+
 
             when(resultValue){
 
                 Constans.Director ->{
                     bindingClass.textView2 .visibility = View.VISIBLE
-                    val tempText = "Получите Вашу зарплату  ${Constans.Director_Salary}"
-                    bindingClass.textView2.text = if (bindingClass.editTextPassword.text.toString() == Constans.Director_Password)
-                        tempText
-                    else
-                        "Не верный пароль"
+                    val tempText = "Деректор получите Вашу зарплату ${Constans.Director_Salary} $"
+                     if (bindingClass.editTextPassword.text.toString() == Constans.Director_Password) {
+                         bindingClass.textView2.text = tempText
+                         bindingClass.imPhoto.setImageResource(R.drawable.myavatar_4)
+                     }
+                    else {
+                        bindingClass.imPhoto.setImageResource(R.drawable.polis)
+                         bindingClass.textView2.text = "Не верный пароль"
+                     }
                 }
 
                 Constans.Enginer ->{
                     bindingClass.textView2.visibility = View.VISIBLE
-                    val tempText = "Получите Ваши ${Constans.Enginer_Salary}"
-                    bindingClass.textView2.text = if (bindingClass.editTextPassword.text.toString() == Constans.Enginer_Password)
-                        tempText
-                    else
-                        "Не верный пароль"
+                    val tempText = " Инженер получите Ваши ${Constans.Enginer_Salary} $"
+                    if (bindingClass.editTextPassword.text.toString() == Constans.Enginer_Password) {
+                        bindingClass.textView2.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.myavatar_3)
+
+                    }
+                    else {
+                        bindingClass.textView2.text = "Не верный пароль"
+                        bindingClass.imPhoto.setImageResource(R.drawable.polis)
+                    }
                 }
 
                 Constans.Driver ->{
                     bindingClass.textView2.visibility = View.VISIBLE
-                    val tempText = "Получите Ваши ${Constans.Driver_Salary}"
-                    bindingClass.textView2.text = if (bindingClass.editTextPassword.text.toString() == Constans.Director_Password)
-                        tempText
-                    else
-                        "Не верный пароль"
+                    val tempText = " Водитель получите Ваши ${Constans.Driver_Salary}$"
+                     if (bindingClass.editTextPassword.text.toString() == Constans.Director_Password) {
+                         bindingClass.textView2.text = tempText
+                         bindingClass.imPhoto.setImageResource(R.drawable.myavatar_2)
+
+                     }
+
+                    else {
+                         bindingClass.textView2.text ="Не верный пароль"
+                         bindingClass.imPhoto.setImageResource(R.drawable.polis)
+                     }
                 }
 
                 Constans.PMHead ->{
                     bindingClass.textView2.visibility = View.VISIBLE
-                    val tempText = "Получите Ваши ${Constans.PMHead_Salary}"
-                    bindingClass.textView2.text = if (bindingClass.editTextPassword.text.toString() == Constans.PMHead_Password)
-                        tempText
-                    else
-                        "Не верный пароль"
+                    val tempText = "ПМ получите Ваши ${Constans.PMHead_Salary}$"
+                    if (bindingClass.editTextPassword.text.toString() == Constans.PMHead_Password) {
+                        bindingClass.textView2.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.myavatar_0)
+                    }
+                    else {
+                        bindingClass.textView2.text = "Не верный пароль"
+                        bindingClass.imPhoto.setImageResource(R.drawable.polis)
+                    }
                 }
 
                 Constans.SuperHead ->{
                     bindingClass.textView2.visibility = View.VISIBLE
-                    val tempText = "Получите Ваши ${Constans.SuperHead_Salary}"
-                    bindingClass.textView2.text = if (bindingClass.editTextPassword.text.toString() == Constans.SuperHead_Password)
-                        tempText
-                    else
-                        "Не верный пароль"
+                    val tempText = "Глава департамента получите Ваши ${Constans.SuperHead_Salary}$"
+                    if (bindingClass.editTextPassword.text.toString() == Constans.SuperHead_Password) {
+                        bindingClass.textView2.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.myavatar_1)
+
+                    }
+                    else {
+                        bindingClass.textView2.text = "Не верный пароль"
+                        bindingClass.imPhoto.setImageResource(R.drawable.polis)
+                    }
                 }
 
                 else ->{
                     bindingClass.textView2.visibility = View.VISIBLE
                     bindingClass.textView2.text ="Вы у нас не работаете"
+                    bindingClass.imPhoto.setImageResource(R.drawable.polis)
 
                 }
             }
